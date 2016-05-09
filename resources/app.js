53c4bb512a5d7a92783f559224aaf89a8670032d
Panteleyev <gleb.panteleyev@mail.ru>
2016-05-10 02:52:50 +0400

    End v5.2 Test 'Refactoring into a Module'

--- resources/app.js
+++ resources/app.js
@@ -1,5 +1,5 @@
 (function () {
-	var app = angular.module('gemStore', []);
+	var app = angular.module('gemStore', ['store-directives']);
 
 	app.controller('StoreController', function () {
 		this.products = gems;
@@ -16,59 +16,6 @@
 
 	});
 
-	app.directive("productDescription", function () {
-		return {
-			restrict:    'E',
-			templateUrl: "product-description.html"
-		};
-	});
-
-	app.directive("productReviews", function () {
-		return {
-			restrict:    'E',
-			templateUrl: "product-reviews.html"
-		};
-	});
-
-	app.directive("productSpecs", function () {
-		return {
-			restrict:    "A",
-			templateUrl: "product-specs.html"
-		};
-	});
-
-	app.directive("productTabs", function () {
-		return {
-			restrict:     "E",
-			templateUrl:  "product-tabs.html",
-			controller:   function () {
-				this.tab = 1;
-
-				this.isSet = function (checkTab) {
-					return this.tab === checkTab;
-				};
-
-				this.setTab = function (activeTab) {
-					this.tab = activeTab;
-				};
-			},
-			controllerAs: "tab"
-		};
-	});
-
-	app.directive("productGallery", function () {
-		return {
-			restrict:     "E",
-			templateUrl:  "product-gallery.html",
-			controller:   function () {
-				this.current    = 0;
-				this.setCurrent = function (imageNumber) {
-					this.current = imageNumber || 0;
-				};
-			},
-			controllerAs: "gallery"
-		};
-	});
 	var gems = [
 		{
 			name:        'Azurite',
