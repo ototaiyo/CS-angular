b80a8db5cbdce76c9202da04fddad8b1c9541e57
Panteleyev <gleb.panteleyev@mail.ru>
2016-05-10 01:54:29 +0400

    End v4.7 Test 'Refactoring Product Gallery'

--- resources/app.js
+++ resources/app.js
@@ -1,10 +1,17 @@
 (function () {
 	var app = angular.module('gemStore', []);
 
-	app.controller('GalleryController', function () {
-		this.current    = 0;
-		this.setCurrent = function (imageNumber) {
-			this.current = imageNumber || 0;
+	app.directive("productGallery", function () {
+		return {
+			restrict:     'E',
+			templateUrl:  "product-gallery.html",
+			controller:   function () {
+				this.current    = 0;
+				this.setCurrent = function (imageNumber) {
+					this.current = imageNumber || 0;
+				};
+			},
+			controllerAs: "gallery"
 		};
 	});
 
