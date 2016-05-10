813b967f71e97f6ce8cd03a3bddb7283e13c0cbf
Panteleyev <gleb.panteleyev@mail.ru>
2016-05-10 04:09:43 +0400

    End v5.4 Test 'Built-in Angular Services'

--- resources/app.js
+++ resources/app.js
@@ -1,11 +1,13 @@
 (function () {
   var app = angular.module('gemStore', [ 'store-directives' ]);
 
-  app.controller('StoreController', function () {
+  app.controller('StoreController', [ '$http', function ($http) {
     var store      = this;
     store.products = [];
-
-  });
+    $http.get('resources/store-products.json').success(function (data) {
+      store.products = data;
+    });
+  } ]);
 
   app.controller('ReviewController', function () {
     this.review = {};
